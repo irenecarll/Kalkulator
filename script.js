047c5b3574ed3c60c4060bc2ec175249a04bea86
irenecarll <irene.carllewis@gmail.com>
2024-09-09 21:02:46 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function tambah(id, input1, input2) {
+    document.getElementById(id).value = Number(input1) + Number(input2);
+
+}
+
+function kurang(id, input1, input2) {
+    document.getElementById(id).value = Number(input1) - Number(input2);
+}
+
+function kali(id, input1, input2) {
+    document.getElementById(id).value = Number(input1) * Number(input2);
+}
+
+function bagi(id, input1, input2) {
+    document.getElementById(id).value = Number(input1) / Number(input2);
+}
+
